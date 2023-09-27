95ac14cc20de457bd1e87da6427a040e1f5fd835
Aaron Sala <salaaron2@gmail.com>
2023-09-27 19:22:50 +0300

    edited

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ let imgBox =document.getElementById("imgbox")
 let qrImage =document.getElementById("qrImage")
 let qrText =document.getElementById("qrText")
 let qrBtn =document.getElementById("qrBtn")
+let links =document.querySelector("#linker")
+let links2 = document.get
 
 
 
 function generateQr(){
 qrImage.src='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+qrText.value;
 
- imgBox.classList.add("show-img");
+ //imgBox.classList.add("show-img");
+ qrBtn.style.background="green";
+ links.innerHTML= qrText.value;
 }
 
 
